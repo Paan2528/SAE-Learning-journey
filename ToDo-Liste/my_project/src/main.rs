677fa9cb657e884c3e944cf04fc7ba759da05cba
Paan2528 <140539606+Paan2528@users.noreply.github.com>
2026-06-07 09:29:27 +0200

    TODO LIST

---
+++ ToDo-Liste/my_project/src/main.rs
@@ -0,0 +1,46 @@
+
+//fn todo_input(todo_list: &str){
+//    println!("{}", todo_list );}
+// press '+' for add todo list
+// press '-' for remove todo list to finisch box
+// press 'p' for add todo to the on process box
+use std::io;
+use std::collections::VecDeque;
+
+fn todo_List() -> io::Result<()>{
+    let mut todo_collection: VecDeque<String> = VecDeque::new();
+    loop {
+
+        let mut todo_input = String::new();
+        io::stdin().read_line(&mut todo_input)?;
+        
+        let trimmed = todo_input.trim();
+       
+        //-------- Vec--------------------// 
+        if trimmed == "exit"{
+            break;
+        }else if !trimmed.is_empty(){
+            todo_collection.push_back(trimmed.to_string());
+        }else if trimmed == "Done" {
+            todo_collection.pop_front();
+        };
+        println!("- {:?}", todo_collection);
+    
+
+    }
+    Ok(())  
+        
+}
+
+//.push  -> add
+//.pop -> remove
+
+fn main() {
+    println!("----- TO DO -----");
+    println!("Please fill in the items you wish to do.");
+    todo_List();
+    
+    if let Err(e) = todo_List(){
+        println!("Error: {}", e);
+    }
+}
