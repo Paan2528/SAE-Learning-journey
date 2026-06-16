5e1c42a85e81adec6aacba61349b9d7bef02ca36
Paan2528 <140539606+Paan2528@users.noreply.github.com>
2026-06-16 17:59:18 +0200

    feat: add feature data check before the data record to HashMap and if does't have it , record it as a new data.

--- mitarbeite_data/hello_rust/src/main.rs
+++ mitarbeite_data/hello_rust/src/main.rs
@@ -1,33 +1,41 @@
 use std::collections::HashMap;
+use std::io;
+use std::ptr::read_volatile;
 
 #[derive(Hash, Eq, PartialEq, Debug)]
 struct DataMitarbeit {
     name: String,
     rolle: String,
 }
+
 // build data
-fn main(){
-    let data_Mitarbeit = HashMap::from([
-    (
-        DataMitarbeit{
-            name: "Janjira".to_string(),
-            rolle: "marketing".to_string(),
-        },
-        30,
-    ),
-]);
+fn main() {
+    let mut data_Mitarbeit = HashMap::new();
+    loop {
+        let mut name = String::new();
+        let mut rolle = String::new();
 
+        println!("Name:");
+        io::stdin().read_line(&mut name).unwrap();
 
+        println!("Rolle:");
+        io::stdin().read_line(&mut rolle).unwrap();
 
-//check
-let check_data = DataMitarbeit{
-    name: "Janjira".to_string(),
-    rolle: "marketing".to_string(),
-}; 
-    if data_Mitarbeit.contains_key(&check_data){
-        println!("{:?}", data_Mitarbeit.get(&check_data));
-    }else {
-        println!("Not Found")
+        //check
+        let person = DataMitarbeit {
+            name: name.trim().to_string(),
+            rolle: rolle.trim().to_string(),
+        };
+
+        if data_Mitarbeit.contains_key(&person) {
+            println!(
+                "You are {}! and your work is {}! Your data was recoding",
+                name, rolle
+            );
+        } else {
+            data_Mitarbeit.insert(person, 30);
+            println!("You're new hier!");
+        }
     }
-println!("{:?}", data_Mitarbeit.contains_key(&check_data));
+    return;
 }
