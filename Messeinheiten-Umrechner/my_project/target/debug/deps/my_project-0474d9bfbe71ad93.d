25cf475ae6fbaaf54ba68dbd9eb02e9cbe0c11f3
Paan2528 <140539606+Paan2528@users.noreply.github.com>
2026-06-23 11:48:28 +0200

    Messeinheiten-Umrechner

---
+++ Messeinheiten-Umrechner/my_project/target/debug/deps/my_project-0474d9bfbe71ad93.d
@@ -0,0 +1,5 @@
+/Users/yatoum/Documents/Taschenrecner/SAE-Learning-journey/Messeinheiten-Umrechner/my_project/target/debug/deps/my_project-0474d9bfbe71ad93.d: src/main.rs
+
+/Users/yatoum/Documents/Taschenrecner/SAE-Learning-journey/Messeinheiten-Umrechner/my_project/target/debug/deps/libmy_project-0474d9bfbe71ad93.rmeta: src/main.rs
+
+src/main.rs:
---
+++ Messeinheiten-Umrechner/my_project/target/debug/deps/my_project-5ae1c87def4aac36.d
@@ -0,0 +1,5 @@
+/Users/yatoum/Documents/Taschenrecner/SAE-Learning-journey/Messeinheiten-Umrechner/my_project/target/debug/deps/my_project-5ae1c87def4aac36.d: src/main.rs
+
+/Users/yatoum/Documents/Taschenrecner/SAE-Learning-journey/Messeinheiten-Umrechner/my_project/target/debug/deps/my_project-5ae1c87def4aac36: src/main.rs
+
+src/main.rs:
